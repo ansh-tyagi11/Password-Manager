fd1897ada90d2471e35ce957cd865117123dfa78
Ansh Tyagi <officialanshtyagi112@gmail.com>
2025-09-28 23:13:32 +0530

    Update Navbar.jsx

--- local-app/src/components/Navbar.jsx
+++ local-app/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
     return (
         <nav className='h-[10vh] bg-red-200 max-w-[100vw]'>
             <div className="flex items-center md:pl-[160px] md:ml-[5vw] justify-center md:justify-start">
-                <img src="/Password-Manager/logo.png" alt="MyPasswordVault Logo" className='mt-[-36px] ml-[-30px] max-h-40 w-auto m-0' />
+                <img src="/logo.png" alt="MyPasswordVault Logo" className='mt-[-36px] ml-[-30px] max-h-40 w-auto m-0' />
             </div>
         </nav>
     )
